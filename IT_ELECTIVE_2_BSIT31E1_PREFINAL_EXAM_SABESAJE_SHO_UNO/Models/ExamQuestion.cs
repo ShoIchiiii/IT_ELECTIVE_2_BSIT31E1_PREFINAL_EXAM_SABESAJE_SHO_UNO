472414e84797fe11e3aa748988c982ca19a25cb9
Sho Uno Sabesaje <155436471+ShoIchiiii@users.noreply.github.com>
2026-09-11 15:33:34 +0800

    Add project files.

---
+++ IT_ELECTIVE_2_BSIT31E1_PREFINAL_EXAM_SABESAJE_SHO_UNO/Models/ExamQuestion.cs
@@ -0,0 +1,11 @@
+﻿namespace IT_ELECTIVE_2_SECTION_PREFINAL_EXAM_Lastname_FirstName.Models
+{
+    public class ExamQuestion
+    {
+        public int Id { get; set; }
+        public string QuestionText { get; set; } = string.Empty;
+        public List<string> Options { get; set; } = new List<string>();
+        public string CorrectAnswer { get; set; } = string.Empty;
+        public string Explanation { get; set; } = string.Empty;
+    }
+}
